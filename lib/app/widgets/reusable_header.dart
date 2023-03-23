05d039ffc043b4a0518cd2add6ca57e4513663a5
MYusufEfendi <yusuf4587@gmail.com>
2023-03-23 11:58:03 +0700

    FINISH:
    - add ic_option_header.svg
    - edit to add actiobn icon on reusable_header.dart

--- lib/app/widgets/reusable_header.dart
+++ lib/app/widgets/reusable_header.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 import '../../constant/colors.dart';
 import '../../constant/strings.dart';
 
@@ -6,9 +8,13 @@ class Header extends StatelessWidget {
   const Header({
     Key? key,
     required this.rightText,
+    this.showIcon = false,
+    this.ontapIcon,
   }) : super(key: key);
 
   final String rightText;
+  final bool showIcon;
+  final Function? ontapIcon;
 
   @override
   Widget build(BuildContext context) {
@@ -22,25 +28,43 @@ class Header extends StatelessWidget {
               Navigator.of(context).pop();
             },
             style: ElevatedButton.styleFrom(
-              shape: const CircleBorder(),
-              padding: const EdgeInsets.all(11),
-              minimumSize: Size.zero,
-              primary: ColorApp.white,
-              elevation: 0
-            ),
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(11),
+                minimumSize: Size.zero,
+                primary: ColorApp.white,
+                elevation: 0),
             child: const Icon(
               Icons.arrow_back,
               size: 18,
               color: ColorApp.black_arrow_back,
             ),
           ),
-          Text(
-            rightText,
-            style: const TextStyle(
-                color: ColorApp.black,
-                fontSize: 16,
-                fontWeight: FontWeight.w600),
-          ),
+          showIcon
+              ? ElevatedButton(
+                  onPressed: () {
+                    ontapIcon!();
+                  },
+                  style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(11),
+                      minimumSize: Size.zero,
+                      primary: ColorApp.white,
+                      elevation: 0),
+                  child: SvgPicture.asset(
+                    'assets/icons/ic_option_header.svg',
+                    fit: BoxFit.fill,
+                    alignment: Alignment.centerLeft,
+                    width: 18,
+                    height: 18,
+                  ),
+                )
+              : Text(
+                  rightText,
+                  style: const TextStyle(
+                      color: ColorApp.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600),
+                ),
         ],
       ),
     );
