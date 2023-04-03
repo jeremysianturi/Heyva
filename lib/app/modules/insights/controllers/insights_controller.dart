10d77d11b77784987933ed05b66a03f048d1c481
MYusufEfendi <yusuf4587@gmail.com>
2023-04-03 22:11:28 +0700

    FINISH:
    - revision insights_view.dart with opacity

--- lib/app/modules/insights/controllers/insights_controller.dart
+++ lib/app/modules/insights/controllers/insights_controller.dart
@@ -1,15 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 class InsightsController extends GetxController {
-
-  TextEditingController emailC = TextEditingController();
-  TextEditingController passC = TextEditingController();
-
-  @override
-  void onClose() {
-    emailC.dispose();
-    passC.dispose();
-    super.onClose();
-  }
+  var currentIndex = 1.obs;
 }
