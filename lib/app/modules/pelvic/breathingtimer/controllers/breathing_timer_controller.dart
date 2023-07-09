edd315e7b6007c21807768a61a2c0879620ed3b5
MYusufEfendi <yusuf4587@gmail.com>
2023-07-09 14:07:47 +0700

    FINISH:
    - add voice to pelvic
    - add gif to pelvic

--- lib/app/modules/pelvic/breathingtimer/controllers/breathing_timer_controller.dart
+++ lib/app/modules/pelvic/breathingtimer/controllers/breathing_timer_controller.dart
@@ -1,19 +1,80 @@
 import 'dart:async';
 
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:heyva/app/modules/pelvic/breathingfinish/views/breathing_finish_view.dart';
 
 class BreathingTimerController extends GetxController {
   var progressValue = 0.0.obs;
+  AudioPlayer? audioPlayer;
+  var playing = false.obs;
+  var sliderProgress = 0.0.obs;
+  var playProgress = 0.obs;
+  var max_value = 10.0.obs;
+  var isTap = false.obs;
+  double _start = 0.0;
 
   @override
   void onInit() {
-    startTimer();
+    // startTimer();
+    onPlay();
     super.onInit();
   }
 
+  onPlay() {
+    if (audioPlayer == null) {
+      audioPlayer = AudioPlayer()..play(AssetSource('pelvic_floor.wav'));
+      playing.value = true;
+      //
+      audioPlayer?.onDurationChanged.listen((Duration event) {
+        // max_value.value = event.inMilliseconds.toDouble();
+        _start = event.inSeconds.toDouble();
+      });
+      List<int> prevPositions = [0, 0];
+      int lostPositions = 0;
+      audioPlayer?.onPositionChanged.listen((Duration event) {
+        if (isTap.isTrue) return;
+
+        int positionChanged = event.inMilliseconds;
+        if (prevPositions[1] > positionChanged) {
+          int diff = prevPositions[1] - prevPositions[0];
+          int tempNext = prevPositions[1] + diff;
+          lostPositions += tempNext - positionChanged;
+        }
+        int correctPosition = positionChanged + lostPositions;
+        // sliderProgress.value = correctPosition.toDouble();
+        progressValue.value = (correctPosition / 1000) / _start;
+        debugPrint("correct position ${(correctPosition.toDouble()) / _start}");
+        if (progressValue.value == 1) {
+          Future.delayed(2.seconds, () {
+            Get.to(const BreathingFinishView());
+          });
+        }
+        prevPositions.add(positionChanged);
+        prevPositions.removeAt(0);
+      });
+
+      audioPlayer?.onPlayerStateChanged.listen((PlayerState state) {
+        playing.value = state == PlayerState.playing;
+      });
+    } else {
+      audioPlayer?.resume();
+    }
+  }
+
+  @override
+  void dispose() {
+    playing.value = false;
+    audioPlayer?.dispose();
+    // timer.cancel();
+
+    super.dispose();
+  }
+
   late Timer timer;
-  final int _start = 16000;
+
+  // final int _start = 16000;
 
   void startTimer() {
     const oneSec = Duration(milliseconds: 1);
--- lib/app/modules/pelvic/breathingtimer/views/breathing_timer_view.dart
+++ lib/app/modules/pelvic/breathingtimer/views/breathing_timer_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:heyva/app/modules/pelvic/breathingfinish/views/breathing_finish_view.dart';
-import 'package:lottie/lottie.dart';
 
 import '../../../../../constant/colors.dart';
 import '../../../../../constant/strings.dart';
@@ -14,64 +13,73 @@ class BreathingTimerView extends GetView<BreathingTimerController> {
   Widget build(BuildContext context) {
     Get.put(BreathingTimerController());
 
-    return Scaffold(
-      body: Container(
-        height: Get.height,
-        width: Get.width,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/bg_breating_exercise.png"),
-            fit: BoxFit.fill,
-          ),
-        ),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 67,
-            ),
-            Obx(() {
-              debugPrint("animate ${controller.progressValue.value}");
-              return Container(
-                margin: EdgeInsets.symmetric(horizontal: 20),
-                width: Get.width,
-                height: 2,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  child: LinearProgressIndicator(
-                    value: controller.progressValue.value,
-                    valueColor:
-                        AlwaysStoppedAnimation<Color>(ColorApp.blue_container),
-                    backgroundColor: ColorApp.grey_font,
-                  ),
-                ),
-              );
-            }),
-            Container(
-              margin: EdgeInsets.only(right: 20, top: 17),
-              width: Get.width,
-              alignment: Alignment.centerRight,
-              child: GestureDetector(
-                onTap: () {
-                  controller.timer.cancel();
-                  Get.to(const BreathingFinishView());
-                },
-                child: const Text(
-                  Strings.skip,
-                  style: TextStyle(
-                      color: ColorApp.blue_container,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16),
-                ),
+    return WillPopScope(
+        onWillPop: () async {
+          controller.dispose();
+          Get.back();
+          return false;
+        },
+        child: Scaffold(
+          body: Container(
+            height: Get.height,
+            width: Get.width,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/bg_breating_exercise.png"),
+                fit: BoxFit.fill,
               ),
             ),
-            Expanded(
-                child: Align(
-              alignment: Alignment.center,
-              child: Lottie.asset("assets/images/pulse_new.json"),
-            ))
-          ],
-        ),
-      ),
-    );
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 67,
+                ),
+                Obx(() {
+                  debugPrint("animate ${controller.progressValue.value}");
+                  return Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    width: Get.width,
+                    height: 2,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      child: LinearProgressIndicator(
+                        value: controller.progressValue.value,
+                        valueColor: AlwaysStoppedAnimation<Color>(
+                            ColorApp.blue_container),
+                        backgroundColor: ColorApp.grey_font,
+                      ),
+                    ),
+                  );
+                }),
+                Container(
+                  margin: EdgeInsets.only(right: 20, top: 17),
+                  width: Get.width,
+                  alignment: Alignment.centerRight,
+                  child: GestureDetector(
+                    onTap: () {
+                      // controller.timer.cancel();
+                      // controller.audioPlayer?.stop();
+                      controller.dispose();
+                      Get.to(const BreathingFinishView());
+                    },
+                    child: const Text(
+                      Strings.skip,
+                      style: TextStyle(
+                          color: ColorApp.blue_container,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+                Expanded(
+                    child: Align(
+                  alignment: Alignment.center,
+                  // child: Lottie.asset("assets/images/data.json"),
+                child: Image.asset("assets/images/animation_wave.gif"),
+                    ))
+              ],
+            ),
+          ),
+        ));
   }
 }
